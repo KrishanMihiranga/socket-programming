9479fcd18d11d418c663f62456e5a629e38e4041
KrishanMihiranga <xp3cto95@gmail.com>
2023-06-13 11:26:41 +0530

    passing string

--- socket-programming/client/src/Main.java
+++ socket-programming/client/src/Main.java
@@ -1,6 +1,23 @@
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
 public class Main {
     
     public static void main(String[] args) {
+
+        try {
+
+            Socket socket = new Socket("localhost", 3002);
+            DataOutputStream dataOutputStream =new DataOutputStream(socket.getOutputStream());
+
+            dataOutputStream.writeUTF("hola");
+            
+            socket.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 }
--- socket-programming/server/src/Main.java
+++ socket-programming/server/src/Main.java
@@ -1,5 +1,27 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
+public class Main {
+
+    public static void main(String[] args) {
+        try {
+
+            //creating a server socket
+            ServerSocket serverSocket= new ServerSocket(3002);
+            //accept request and move to new socket
+            Socket socket = serverSocket.accept();
+            //Java application used inputStream to read the data
+            DataInputStream dataInputStream =new DataInputStream(socket.getInputStream());
+
+            String message = dataInputStream.readUTF();
+            System.out.println("Client : " + message);
+
+            socket.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
